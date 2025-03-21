b5458c2c3910268e7ddb7490730a450789c6b1fb
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2025-03-21 17:09:42 +0530

    Landing Page Complete

--- backend/src/config/email.js
+++ backend/src/config/email.js
@@ -17,6 +17,21 @@ export const transporter = nodemailer.createTransport({
   },
 });
 
+// export let transport = nodemailer.createTransport({
+//   service: "Outlook365",
+//   host: "smtp.office365.com",
+//   port: "587",
+//   secure: false, // Required for STARTTLS
+//   tls: {
+//     ciphers: "SSLv3",
+//     rejectUnauthorized: false, // Prevents certificate validation errors
+//   },
+//   auth: {
+//     user: process.env.EMAIL_ID, // Your Outlook email
+//     pass: process.env.PASSWORD_EMAIL, // Use App Password if needed
+//   },
+// });
+
 transporter
   .verify()
   .then(() => console.log("Email server ready to send messages"))
