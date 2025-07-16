497b111075cf34d0c2cbdea5104528aefc260f43
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2025-07-16 12:42:52 +0530

    Speaker-register.html -> paper.html

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,8 +26,8 @@ app.use(cors({
     "http://localhost:3000",
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://icmmcs.org",
-    "http://127.0.0.1:3000/"
+    "https://www.icmmcs.org",
+    "http://127.0.0.1:3000"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "Accept", "Origin", "X-Requested-With"],
